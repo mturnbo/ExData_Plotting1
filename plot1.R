eb0557744b8e10c7ff0d2febff7e485b17706931
Marcus E. Turnbo <marcus@emarcus.com>
2014-10-12 16:43:01 -0500

    Created Assignment 1 - Plot 2

--- plot1.R
+++ plot1.R
@@ -2,7 +2,7 @@
 
 # read and format data
 print("Reading Data")
-hpc <- read.table("../../data/household_power_consumption.txt", sep=";", header=T, na.strings="?")
+hpc <- read.table("household_power_consumption.txt", sep=";", header=T, na.strings="?")
 hpc <- hpc[hpc$Date %in% c("1/2/2007","2/2/2007"),]
 hpc$DateTime <- paste(hpc$Date, hpc$Time, sep=" ")
 hpc$DateTime <- strptime(hpc$DateTime, "%d/%m/%Y %H:%M:%S")
