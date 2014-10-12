6847157695c68fe5da7e181fe1ed98b7c79f93d6
Marcus E. Turnbo <marcus@emarcus.com>
2014-10-12 16:21:19 -0500

    Created Assignment 1 - Plot 1

---
+++ plot1.R
@@ -0,0 +1,12 @@
+# Assignment 1 - Plot 1
+
+# read and format data
+print("Reading Data")
+hpc <- read.table("../../data/household_power_consumption.txt", sep=";", header=T, na.strings="?")
+hpc <- hpc[hpc$Date %in% c("1/2/2007","2/2/2007"),]
+hpc$DateTime <- paste(hpc$Date, hpc$Time, sep=" ")
+hpc$DateTime <- strptime(hpc$DateTime, "%d/%m/%Y %H:%M:%S")
+
+# generate plot
+print("Generating Plot")
+hist(hpc$Global_active_power, main="Global Active Power", col="Red", xlab="Global Active Power (killowatts)", ylab="Frequency")
